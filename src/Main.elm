978fc5753c932d42cfeb2eac13bdf80d7b7fafcb
Mimarik <mimihanus2@gmail.com>
2019-08-18 16:46:49 +0000

    Priprav Main na použitie Browser.element

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,37 @@
 import Html exposing (..)
+import Browser
+
+main = Browser.element
+  { init = init
+  , update = update
+  , subscriptions = subscriptions
+  , view = view
+  }
 
 
 -- MODEL
 
-type alias Model = { ... }
+type alias Model = ()
+
+init : () -> (Model, Cmd Msg)
+init _ = ((), Cmd.none)
 
 
 -- UPDATE
 
-type Msg = Reset | ...
+type alias Msg = ()
 
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Reset -> ...
-    ...
+update : Msg -> Model -> (Model, Cmd Msg)
+update _ _ = ((), Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions _ = Sub.none
 
 
 -- VIEW
 
 view : Model -> Html Msg
-view model =
-  ...
+view model = text "Lorem ipsum"
