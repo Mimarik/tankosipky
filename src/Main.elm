4001e25588a816ea4e077061348c25b0067a8491
Mimarik <mimihanus2@gmail.com>
2019-08-19 10:01:03 +0000

    Pridaj počítanie hráčov na začiatku hry

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,12 @@
 import Html exposing (..)
 import Browser
+import String exposing (fromInt)
+
+import Element as El
+import Element.Border as Border
+import Element.Background as Bg
+import Element.Font as Font
+import Element.Events exposing (onClick)
 
 main = Browser.element
   { init = init
@@ -11,18 +18,35 @@ main = Browser.element
 
 -- MODEL
 
-type alias Model = ()
+type alias Model =
+  { hracov : Maybe Int
+  , hrac : Int
+  , prijate : Bool
+  }
 
 init : () -> (Model, Cmd Msg)
-init _ = ((), Cmd.none)
+init _ = (
+  { hracov = Nothing
+  , hrac = 1
+  , prijate = False
+  }, Cmd.none)
 
 
 -- UPDATE
 
-type alias Msg = ()
+type Msg
+  = Prijal
+  | Vsetci
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update _ _ = ((), Cmd.none)
+update msg ({hrac, hracov} as model) = (
+  case msg of
+    Prijal ->
+      case hracov of
+        Nothing -> { model | hrac = hrac + 1 }
+        Just n -> { model | hrac = modBy n (hrac + 1) }
+    Vsetci -> { model | hrac = 0, hracov = Just (hrac + 1) }
+  , Cmd.none)
 
 
 -- SUBSCRIPTIONS
@@ -34,4 +58,24 @@ subscriptions _ = Sub.none
 -- VIEW
 
 view : Model -> Html Msg
-view model = text "Lorem ipsum"
+view {hracov, hrac, prijate} = El.layout [ Font.center ] <|
+  if prijate then
+    El.none
+  else
+    case hracov of
+      Nothing -> El.column [ El.width El.fill, El.height El.fill ]
+        [ El.el (potvrd Prijal) <| El.text ("Som hráč " ++ fromInt hrac)
+        , El.el [ onClick Vsetci ] <|  El.text ("Som hráč 0")
+        ]
+      Just _ -> El.el (potvrd Prijal) <| El.text ("Som hráč " ++ fromInt hrac)
+
+potvrd : msg -> List (El.Attribute msg)
+potvrd msg =
+  [ onClick msg
+  , El.width El.fill
+  , El.height El.fill
+  , El.padding 8
+  , Bg.color (El.rgb 0 0.6 1)
+  , Font.color (El.rgb 1 1 1)
+  , El.pointer
+  ]
